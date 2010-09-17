943ff9bd5d8b5b7fd67010825832845ca29e2e0a
cnauroth <cnauroth@hotmail.com>
2010-09-17 09:08:10 -0700

    Created skeleton Java command-line utility project.

---
+++ src/main/java/cnauroth/ldaputil/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Main.java
+ */
+
+package cnauroth.ldaputil;
+
+/**
+ * This class is the main entry point into java-ldap-util.
+ */
+public final class Main {
+
+    /**
+     * This method is the main entry point.
+     * 
+     * @param args String[] command-line arguments.
+     */
+    public static void main(String[] args) {
+        // TODO: Implement.
+    }
+}
+
